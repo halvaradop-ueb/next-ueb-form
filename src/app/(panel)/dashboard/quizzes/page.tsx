6657caa096f02fca5a22114162fc4a25386f9f02
JFvillarreal <jvillarrealc@unbosque.edu.co>
2025-05-14 10:21:17 -0500

    cambio en preguntas y en base de datos porque se pedia stage, entoces agrego colummna

--- src/app/(panel)/dashboard/quizzes/page.tsx
+++ src/app/(panel)/dashboard/quizzes/page.tsx
@@ -34,7 +34,7 @@ import type { Question, StageService } from "@/lib/@types/services"
 import { addQuestion, deleteQuestion, getQuestions, updateQuestion } from "@/services/questions"
 import { Search, Plus, Pencil, Trash2, AlertCircle } from "lucide-react"
 import { getStages } from "@/services/stages"
-
+import { v4 as uuidv4 } from "uuid"
 const questionTypes: Record<Question["question_type"], string> = {
     text: "Texto",
     single_choice: "Selección única",
@@ -88,7 +88,7 @@ const QuizzesPage = () => {
     const handleCreateQuestion = () => {
         setNewQuestion({
             ...initialState,
-            id: crypto.randomUUID(),
+            id: uuidv4(),
         })
         setTextOptions("")
         setIdleForm("create")
