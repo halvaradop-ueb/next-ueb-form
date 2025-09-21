1f9261556602404608b6f48f5230c77fe745c1fd
halvaradop <hernanvid123@gmail.com>
2025-09-20 21:35:09 -0500

    chore: envs

--- apps/web/src/services/utils.ts
+++ apps/web/src/services/utils.ts
@@ -1,10 +1,7 @@
 const isProduction = process.env.NODE_ENV === "production"
-/** 
+
 export const API_ENDPOINT =
     isProduction && process.env.NEXT_PUBLIC_API_ENDPOINT ? process.env.NEXT_PUBLIC_API_ENDPOINT : "http://localhost:4000/api/v1"
-*/
-
-export const API_ENDPOINT = "https://next-ueb-form-api.vercel.app/api/v1"
 
 export const createRequest = (method: "GET" | "POST" | "PUT" | "DELETE", url: string, body?: any) => {
     const data = typeof body === "object" && !(body instanceof FormData) ? JSON.stringify(body) : body
